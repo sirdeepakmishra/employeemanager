e03fa912eddbaebd06ff9f0142554a9d4556aba1
DEEPAK <sirdeepakmishra@gmail.com>
2021-06-22 19:22:12 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,14 +1,24 @@
 def buildApp() {
-    echo 'building the application...'
-} 
+	echo "building the application.."
+}
 
 def testApp() {
-    echo 'testing the application...'
-} 
+	echo 'testing the application...'
+}
 
 def deployApp() {
-    echo 'deplying the application...'
-    echo "deploying version ${params.VERSION}"
-} 
+	echo "deploying the application...with credentials ${SERVER_CREDENTIALS}"
+	echo "deploying version ${params.VERSION}"
+}
+
+def deployApp2() {
+	echo 'deplying 2 the application...'
+	echo "deploying 2 version ${params.VERSION}"
+	withCredentials([
+		usernamePassword(credentials: 'server-user', usernameVariable: USER, passwordVariable: PWD)
+	]) {
+		echo "some script ${USER} and ${PWD}"
+	}
+}
 
 return this
